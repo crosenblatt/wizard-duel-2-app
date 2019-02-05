ca6eafb92f21295c13d47cdd92e22545382b2479
crosenblatt <rosench13@gmail.com>
2019-02-05 17:46:26 -0500

    deleted some boilerplate code

--- WizardDuel2/app/src/main/java/com/cs307/crosenblatt/wizardduel2/MainActivity.java
+++ WizardDuel2/app/src/main/java/com/cs307/crosenblatt/wizardduel2/MainActivity.java
@@ -2,6 +2,25 @@ package com.cs307.crosenblatt.wizardduel2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.mongodb.lang.NonNull;
+import com.mongodb.stitch.android.core.Stitch;
+import com.mongodb.stitch.android.core.StitchAppClient;
+import com.mongodb.stitch.android.core.auth.StitchUser;
+import com.mongodb.stitch.android.services.mongodb.remote.RemoteMongoClient;
+import com.mongodb.stitch.android.services.mongodb.remote.RemoteMongoCollection;
+import com.mongodb.stitch.core.auth.providers.anonymous.AnonymousCredential;
+import com.mongodb.stitch.core.services.mongodb.remote.RemoteUpdateOptions;
+import com.mongodb.stitch.core.services.mongodb.remote.RemoteUpdateResult;
+
+import org.bson.Document;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
