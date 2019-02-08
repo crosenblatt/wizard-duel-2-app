5f6a68a24687df5e471e21095e39873c51b6ed4e
crosenblatt <rosench13@gmail.com>
2019-02-07 19:18:48 -0500

    added functionality to connect to server

--- WizardDuel2/app/src/main/java/com/cs307/crosenblatt/wizardduel2/GameActivity.java
+++ WizardDuel2/app/src/main/java/com/cs307/crosenblatt/wizardduel2/GameActivity.java
@@ -6,10 +6,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
+
+
 public class GameActivity extends AppCompatActivity {
 
     Button spell1, spell2;
     TextView spellCast;
+    Socket socket;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,10 +24,19 @@ public class GameActivity extends AppCompatActivity {
         spell2 = (Button)findViewById(R.id.button_spell2);
         spellCast = (TextView)findViewById(R.id.spell_cast);
 
+        try {
+            socket = IO.socket("http://10.186.126.109:3000").connect();
+            socket.emit("join");
+        } catch(Exception e) {
+            spellCast.setText("BIG OOF");
+        }
+
+
         spell1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Do something in response to button click
                 spellCast.setText("FIRE!");
+                socket.emit("messagedetection", "FIRE");
             }
         });
 
@@ -30,9 +44,16 @@ public class GameActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // Do something in response to button click
                 spellCast.setText("ICE!");
+                socket.emit("messagedetection", "ICE");
             }
         });
+
+
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 
 }
