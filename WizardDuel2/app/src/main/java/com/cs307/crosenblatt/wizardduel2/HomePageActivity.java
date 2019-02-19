f2f85d50b9e297c369401a793b4797de933a6cb8
Vikas Tatineni <vtatinen@purdue.edu>
2019-02-19 00:52:23 -0500

    Connected Login Screen and Stats Page

--- WizardDuel2/app/src/main/java/com/cs307/crosenblatt/wizardduel2/HomePageActivity.java
+++ WizardDuel2/app/src/main/java/com/cs307/crosenblatt/wizardduel2/HomePageActivity.java
@@ -8,8 +8,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-public class HomePageActivity extends AppCompatActivity {
+import com.github.nkzawa.emitter.Emitter;
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
 
+import org.json.JSONObject;
+
+public class HomePageActivity extends AppCompatActivity {
+    Socket socket;
     Button play_button, stats_button, top_players_button, spellbook_button;
     User user;
 
@@ -24,6 +30,12 @@ public class HomePageActivity extends AppCompatActivity {
         spellbook_button=(Button)findViewById(R.id.spellbook_button);
         user=(User)getIntent().getSerializableExtra("user");
 
+        try {
+            socket = IO.socket("http://128.211.242.3:3000").connect();
+        } catch (Exception e){
+            System.out.println(e.getStackTrace());
+        }
+
         play_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -35,6 +47,38 @@ public class HomePageActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 if(!user.getUsername().equals("GUEST")) {
+                    try {
+                        socket.emit("getUserStats", user.getUsername());
+                        socket.once("statsValid", new Emitter.Listener() {
+                            @Override
+                            public void call(final Object... args) {
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        JSONObject result = (JSONObject) args[0];
+                                        try {
+                                            int success = result.getInt("valid");
+                                            JSONObject userStats = result.getJSONObject("userStats"); // holds user level, rank, eloRating, wins, losses -> pass username to next page yourself.
+                                            /*
+                                            System.out.println(success); // -> Used to test if it is correctly outputting
+                                            if (success == 0) {
+                                                System.out.println(userInfo.getInt("level"));
+                                            }
+                                            */
+                                            //MARCEL HANDLE THESE CASES -> 0 = Valid, 1 = INVALID Username, -1 = SERVER ERROR
+                                            //IF success == 0, then userStats is not empty
+                                        } catch (Exception e) {
+                                            System.out.println(e.getStackTrace());
+                                        }
+
+                                    }
+                                });
+                            }
+                        });
+                    } catch (Exception e) {
+                        // PRINT OUT MESSAGE ABOUT HAVING ERROR CONNECTING TO SERVER
+                    }
+                    // PASS USER STATS TO STATS PAGE
                     goToStatsPage();
                 }else{
                     AlertDialog guest_error = new AlertDialog.Builder(HomePageActivity.this).create();
