5819a7bc3b70ea88f8603dc232066f6051947528
kevintxy <ktaha@purdue.edu>
2019-04-04 23:46:30 -0400

    Tutorial Activity

---
+++ WizardDuel2/app/src/main/java/com/cs307/crosenblatt/wizardduel2/TutorialActivity.java
@@ -0,0 +1,13 @@
+package com.cs307.crosenblatt.wizardduel2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TutorialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutorial);
+    }
+}
