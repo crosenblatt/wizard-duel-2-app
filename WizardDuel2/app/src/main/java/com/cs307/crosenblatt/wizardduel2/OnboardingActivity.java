e00278d1e44e7671f53b1eb924a47cc7f8fe23ba
kevintxy <ktaha@purdue.edu>
2019-04-14 23:16:55 -0400

    First few slides of tutorial

--- WizardDuel2/app/src/main/java/com/cs307/crosenblatt/wizardduel2/OnboardingActivity.java
+++ WizardDuel2/app/src/main/java/com/cs307/crosenblatt/wizardduel2/OnboardingActivity.java
@@ -6,8 +6,9 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 
 import com.cs307.crosenblatt.fragment.OnboardingFragmentOne;
-import com.cs307.crosenblatt.fragment.OnboardingFragmentTwo;
 import com.cs307.crosenblatt.fragment.OnboardingFragmentThree;
+import com.cs307.crosenblatt.fragment.OnboardingFragmentTwo;
+import com.cs307.crosenblatt.fragment.OnboardingFragmentTwo2;
 import com.vlonjatg.android.apptourlibrary.AppTour;
 
 public class OnboardingActivity extends AppTour {
@@ -16,6 +17,7 @@ public class OnboardingActivity extends AppTour {
         int customSlideColor = Color.parseColor("#FF5722");
         addSlide(new OnboardingFragmentOne(), customSlideColor);
         addSlide(new OnboardingFragmentTwo(), customSlideColor);
+        addSlide(new OnboardingFragmentTwo2(), customSlideColor);
         addSlide(new OnboardingFragmentThree(), customSlideColor);
         setSeparatorColor(Color.parseColor("#FF5722"));
         setSkipButtonTextColor(Color.WHITE);
